a8ed8f3386b33b13536f0c831979481201aad7eb
Luis Sanchez <luisanchezdh@gmail.com>
2026-05-25 13:05:35 -0400

    added toggle to isCompleted field in todos

--- src/App.svelte
+++ src/App.svelte
@@ -5,16 +5,40 @@
     isCompleted: boolean;
   }
 
-  let todos: Todo[] = [];
-  let newTodo: string = "";
+  let todos = $state<Todo[]>([]);
+  let newTodo = $state("");
+
   let title = "Todo App";
 
   function addTodo(todo: Todo) {
     todos = [...todos, todo];
+    newTodo = "";
+    console.log(todos);
+  }
+
+  function toggleComplete(e, id: string) {
+    console.log(e);
+
+    todos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: !todo.isCompleted };
+      } else {
+        return todo;
+      }
+    });
   }
 </script>
 
 <h1>{title}</h1>
+
+{#each todos as todo (todo.id)}
+  <li>{todo.name}</li>
+  <input
+    type="checkbox"
+    id={todo.id}
+    onclick={(e) => toggleComplete(e, todo.id)}
+  /><label for="isCompleted">Completed</label>
+{/each}
 
 <input
   type="text"
@@ -23,7 +47,6 @@
     e.key === "Enter" &&
     addTodo({ id: crypto.randomUUID(), name: newTodo, isCompleted: false })}
 />
-<p>{todos}</p>
 
 <style>
   h1 {
